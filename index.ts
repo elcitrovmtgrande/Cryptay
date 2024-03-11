e9974c796b6a8fec400e8ce2350d9a4f579fda27
Victor MAHE <victormahe@ODMAC01598.local>
2024-03-11 10:41:22 +0100

    Lib test

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import ECPairFactory from "ecpair";
 import * as ecc from "tiny-secp256k1";
-import bitcoin from "bitcoinjs-lib";
+import * as bitcoin from "bitcoinjs-lib";
 
 (async () => {
   const ECPair = ECPairFactory(ecc);
   const keyPair = ECPair.makeRandom();
   const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey });
+
+  console.log("address:", address);
 })();
